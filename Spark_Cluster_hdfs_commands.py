52ec2a9617a95b869093802636d59c70a8616958
hanhan <wuhanhan999@gmail.com>
2016-04-20 12:46:23 -0700

    update commands

--- Spark_Cluster_hdfs_commands.py
+++ Spark_Cluster_hdfs_commands.py
@@ -3,14 +3,14 @@ Databrics Cloud (Spark Cluster) File System is built upon HDFS,
 therefore, the commands to operate those files are HDFS commands
 '''
 
-# check all the support commands in a cell
+# find all the support commands
 dbutils.fs.help() 
 
 # remove a folder
-rm("/FileStore/tables", True)
+dbutils.fs.rm("/FileStore/tables", True)
 
 # remove a file
-rm("/FileStore/tables", False)
+dbutils.fs.rm("/FileStore/tables", False)
 
 # list all the folders/files
 display(dbutils.fs.ls("/FileStore/"))
