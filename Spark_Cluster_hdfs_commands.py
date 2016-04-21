a3bf3a8025c48217d3313b6561999778b0767861
hanhan <wuhanhan999@gmail.com>
2016-04-20 17:03:21 -0700

    update commands

--- Spark_Cluster_hdfs_commands.py
+++ Spark_Cluster_hdfs_commands.py
@@ -8,9 +8,15 @@ dbutils.fs.help()
 
 # remove a folder
 dbutils.fs.rm("/FileStore/tables", True)
+# or
+%fs rm -r "/FileStore/tables"
 
 # remove a file
-dbutils.fs.rm("/FileStore/tables", False)
+dbutils.fs.rm("/FileStore/tables/f1", False)
+# or
+%fs rm  "/FileStore/tables/f1"
 
 # list all the folders/files
 display(dbutils.fs.ls("/FileStore/"))
+# or
+%fs ls "/FileStore/"
