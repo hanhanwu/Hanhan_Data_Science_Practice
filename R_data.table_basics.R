75fae45af1420e3ea6e30aaeba03b8675d062ff1
hanhan <wuhanhan999@gmail.com>
2016-05-11 23:35:06 -0700

    update R_data.table_basics.R

--- R_data.table_basics.R
+++ R_data.table_basics.R
@@ -26,3 +26,52 @@ dim(dt2)
 # Select row 2 twice and row 3, returning a data.table with three rows 
 ## where row 2 is a duplicate of row 1.
 dt2[c(2,2,3)]
+
+
+# About Column j
+## create a data table
+DT <- data.table(a=c(1,2,3,4,5), b=LETTERS[1:5], c=c(6,7,8,9,10))
+## select a column and output as a data.table
+DT[,.(b)]
+## select a column and output it as a vector
+DT[,(b)]
+## Note: when using .() in j position, the output is always a data.table
+
+
+## non-existing column
+D <- 5
+DT[, .(D)]  # output 5 as a data.table
+DT[, (D)]  # output 5 as vector
+
+## subsetting data.table
+subDT <- DT[1:3, .(b,c)]  # only get column b,c with the first 3 rows
+ans <- DT[, .(b, val=a*c)]  # val is the product of column a, c
+target <- data.table(b = c("a", "b", "c", "d", "e", "a", "b", "c", "d", "e"), 
+                     val = as.integer(c(6:10, 1:5)))
+# ans2 is the same as target
+ans2 <- DT[, .(b, val=as.integer(c(6:10, 1:5)))]
+
+
+# Column by
+DT <- as.data.table(iris)
+## For each Species, print the mean Sepal.Length
+DT[, mean(Sepal.Length), by=Species]
+## Print mean Sepal.Length, grouping by first letter of Species
+DT[, mean(Sepal.Length), by=substr(Species, 1, 1)]
+
+
+## use .N in j with by, will count number of rows in each group
+
+## Group the specimens by Sepal area (to the nearest 10 cm2) 
+## and count how many occur in each group
+DT[, .(Count = .N), by=.(Area = 10*round(Sepal.Length*Sepal.Width/10))]
+
+## calculate cumulative sum of C in column C while you group by A,B. 
+## Store it in a new data.table DT2 with 3 columns: A, B and C
+set.seed(1L)
+DT <- data.table(A = rep(letters[2:1], each = 4L), 
+                 B = rep(1:4, each = 2L), 
+                 C = sample(8))
+DT2 <- DT[, .(C = cumsum(C)), by = .(A, B)]
+## # Select from DT2 the last two values from C while you group by A
+DT2[, .(C = tail(C, 2)), by = A]
