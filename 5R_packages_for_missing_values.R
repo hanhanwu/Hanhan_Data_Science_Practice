8303eb2e8f2dd66b8697dce8728ec4fca05a8324
hanhan <wuhanhan999@gmail.com>
2016-06-10 23:22:48 -0700

    update 5R_packages_for_missing_values.R

--- 5R_packages_for_missing_values.R
+++ 5R_packages_for_missing_values.R
@@ -4,12 +4,8 @@
 ## MICE assumes that the missing data are Missing at Random (MAR), 
 ## which means that the probability that a value is missing depends 
 ## only on observed value and can be predicted using them.
-## By default, linear regression is used to predict continuous missing values.
-## Logistic regression is used for categorical missing values. 
-## PMM (Predictive Mean Matching)  – For numeric variables
-## logreg(Logistic Regression) – For Binary Variables( with 2 levels)
-## polyreg(Bayesian polytomous regression) – For Factor Variables (>= 2 levels)
-## Proportional odds model (ordered, >= 2 levels)
+## use "?mice" to check different methods applied on different data types
+
 attach(iris)
 summary(iris)
 # generate 10% missing values at random
@@ -26,4 +22,19 @@ mice_plot <- aggr(iris.mis, col=c('green', 'blue'),
                   labels=names(iris.mis), cex.axis=0.7,
                   gap=3, ylab=c("Missing Data", "Pattern"))
 
-
+str(iris.mis)
+# indicates which impute method to use for each column
+imputed_data <- mice(iris.mis, method = c('pmm', 'pmm', 'pmm', 'pmm', 'polyreg'), 
+                     seed=410)
+summary(imputed_data)
+# check imputed values
+imputed_data$imp$Sepal.Width
+# m in the summay of imputed_data indicates the number of data sets
+# here, with complete(), we can choose a certain dataset
+complete_data <- complete(imputed_data, 4)
+summary(complete_data)
+# build predictive model on all the m datasets, use with() method
+fit <- with(data = imputed_data, exp = lm(Sepal.Width ~ Sepal.Length+Sepal.Width))
+# combine results of the analysis on m datasets
+combine <- pool(fit)
+summary(combine)
