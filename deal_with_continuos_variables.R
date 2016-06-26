da18ec8a8502eb58138ee62e6d87cd4769473070
hanhan <wuhanhan999@gmail.com>
2016-06-26 13:48:45 -0700

    update deal_with_continuos_variables.R

--- deal_with_continuos_variables.R
+++ deal_with_continuos_variables.R
@@ -22,3 +22,17 @@ income_sd <- sd(income)*sqrt((length(income)-1)/length(income))
 income_mean <- mean(income)
 get_z <- function(v, my_mean, my_sd) (v-my_mean)/my_sd
 apply(income, 1, get_z, income_mean, income_sd)
+
+
+# method 3 - convert highly skewed variables
+## check skewness 
+library(e1071)
+## in this case, the value indiaates its skewed towards right
+skewness(data$Income)
+skewness(data$Illiteracy)  
+## or simply check histogram
+hist(data$Income)
+hist(data$Illiteracy)
+## data$Illiteracy is highly skewed, change to log values, much better!
+hist(log(data$Illiteracy))
+skewness(log(data$Illiteracy))
