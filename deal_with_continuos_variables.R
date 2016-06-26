dad91a4da69e8b658cb561db5788e5e528171724
hanhan <wuhanhan999@gmail.com>
2016-06-26 13:29:04 -0700

    update deal_with_continuos_variables.R

--- deal_with_continuos_variables.R
+++ deal_with_continuos_variables.R
@@ -12,3 +12,13 @@ bins <- cut(data$Frost, 3, include.lowest = TRUE)
 bins
 bins <- cut(data$Frost, 3, include.lowest = TRUE, labels = c('Low', 'Medium', 'High'))
 bins
+
+
+# method 2 - normalization, Z-score
+## I like this z-score tutorial: http://stats.seandolinar.com/calculating-z-scores-with-r/
+income <- matrix(data$Income)
+hist(income)
+income_sd <- sd(income)*sqrt((length(income)-1)/length(income))
+income_mean <- mean(income)
+get_z <- function(v, my_mean, my_sd) (v-my_mean)/my_sd
+apply(income, 1, get_z, income_mean, income_sd)
