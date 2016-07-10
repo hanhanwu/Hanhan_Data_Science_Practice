639d949a30193e996d4b565e9942b6a9a23db877
hanhan <wuhanhan999@gmail.com>
2016-07-09 23:54:57 -0700

    update minimize_logloss.R

--- minimize_logloss.R
+++ minimize_logloss.R
@@ -22,7 +22,32 @@ train$Made.Donation.in.March.2007 <- as.factor(train$Made.Donation.in.March.2007
 set.seed(410)
 cv_train_rows <- sample(nrow(train), floor(nrow(train)*0.85))
 cv_train <- train[cv_train_rows,]
-cv_test <- test[-cv_train_rows,]
+cv_test <- train[-cv_train_rows,]
 
 ## Note: In real world, we need to do more feature engineering before the following operations!
 
+library(randomForest)
+model_rf <- randomForest(Made.Donation.in.March.2007~., data = cv_train, keep.forest = T, importance = T)
+cv_predict <- as.data.frame(predict(model_rf, type = "prob"))
+str(cv_predict)
+
+# predefine LogLoss function
+LogLoss<-function(act, pred)
+{
+  eps = 1e-15;
+  nr = length(pred)
+  pred = matrix(sapply( pred, function(x) max(eps,x)), nrow = nr) 
+  pred = matrix(sapply( pred, function(x) min(1-eps,x)), nrow = nr)
+  ll = sum(act*log(pred) + (1-act)*log(1-pred))
+  ll = ll * -1/(length(act)) 
+  return(ll);
+}
+
+# calculate logloss wihtout Platt Scaling
+LogLoss(as.numeric(as.character(cv_test$Made.Donation.in.March.2007)), cv_predict$`1`)
+## 11.9268
+
+# using Platt Scaling
+df <- data.frame(cv_predict$`1`, cv_train$Made.Donation.in.March.2007)
+colnames(df) <- c("predict_value", "actual_value")
+model_LogisticRegresion <- glm(actual_value~predict_value, data = df, family = binomial)
