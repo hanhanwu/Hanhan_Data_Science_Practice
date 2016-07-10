4df1f2878f140c7301bfebee2062c4257db3f8e2
hanhan <wuhanhan999@gmail.com>
2016-07-10 12:52:54 -0700

    update minimize_logloss.R

--- minimize_logloss.R
+++ minimize_logloss.R
@@ -118,3 +118,75 @@ predict_test_platt <- predict(model_LogisticRegresion, df_test, type = "response
 summary(predict_test_platt)
 str(predict_test_platt)
 
+
+##************************Isotonic Regression***************************##
+
+fit.isoreg <- function(iso, x0) 
+{
+  o = iso$o
+  if (is.null(o)) 
+    o = 1:length(x)
+  x = iso$x[o]
+  y = iso$yf
+  ind = cut(x0, breaks = x, labels = FALSE, include.lowest = TRUE)
+  min.x <- min(x)
+  max.x <- max(x)
+  adjusted.knots <- iso$iKnots[c(1, which(iso$yf[iso$iKnots] > 0))]
+  fits = sapply(seq(along = x0), function(i) {
+    j = ind[i]
+    
+    # Handles the case where unseen data is outside range of the training data
+    if (is.na(j)) {
+      if (x0[i] > max.x) j <- length(x)
+      else if (x0[i] < min.x) j <- 1
+    }
+    
+    # Find the upper and lower parts of the step
+    upper.step.n <- min(which(adjusted.knots > j))
+    upper.step <- adjusted.knots[upper.step.n]
+    lower.step <- ifelse(upper.step.n==1, 1, adjusted.knots[upper.step.n -1] )
+    
+    # Perform a liner interpolation between the start and end of the step
+    denom <- x[upper.step] - x[lower.step] 
+    denom <- ifelse(denom == 0, 1, denom)
+    val <- y[lower.step] + (y[upper.step] - y[lower.step]) * (x0[i] - x[lower.step]) / (denom)
+    
+    # Ensure we bound the probabilities to [0, 1]
+    val <- ifelse(val > 1, max.x, val)
+    val <- ifelse(val < 0, min.x, val)
+    val <- ifelse(is.na(val), max.x, val) # Bit of a hack, NA when at right extreme of distribution
+    val
+  })
+  fits
+}
+
+
+# remove the duplicated predict values
+duplicated_idx <- duplicated(cv_predict$`1`)
+cv_predict_unique <- cv_predict$`1`[!duplicated_idx]
+
+cv_train$Made.Donation.in.March.2007 <- as.numeric(as.character(cv_train$Made.Donation.in.March.2007))
+cv_actual_unique <- cv_train$Made.Donation.in.March.2007[!duplicated_idx]
+
+model_iso <- isoreg(cv_predict_unique, cv_actual_unique)
+
+plot(model_iso,plot.type = "row")
+
+cv_predict_isotonic <- fit.isoreg(model_iso, cv_predict$`1`)
+LogLoss(as.numeric(as.character(cv_test$Made.Donation.in.March.2007)), cv_predict_isotonic)
+# 11.53349
+
+# reliability plot, the line closer to the grey line, more accurate (less logloss)
+plot(c(0,1),c(0,1), col="grey",type="l",xlab = "Mean Prediction",ylab="Observed Fraction")
+k1<-reliability.plot(as.numeric(as.character(cv_train$Made.Donation.in.March.2007)),cv_predict$`1`,bins = 5)
+lines(k1$V2, k1$V1, xlim=c(0,1), ylim=c(0,1), xlab="Mean Prediction", ylab="Observed Fraction", col="red", type="o", main="Reliability Plot")
+
+k2<-reliability.plot(as.numeric(as.character(cv_train$Made.Donation.in.March.2007)),cv_predict_isotonic,bins = 5)
+lines(k2$V2, k2$V1, xlim=c(0,1), ylim=c(0,1), xlab="Mean Prediction", ylab="Observed Fraction", col="blue", type="o", main="Reliability Plot")
+
+legend("topright",lty=c(1,1),lwd=c(2.5,2.5),col=c("blue","red"),legend = c("isotonic scaling","without isotonic scaling"))
+
+# make prediction on test data
+predict_test_isotonic<-as.data.frame(fit.isoreg(model_iso,df_test$predict_value))
+summary(predict_test_isotonic)
+str(predict_test_isotonic)
