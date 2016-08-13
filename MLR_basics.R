6bd219ffd924f3f4996e51285d0ad22af443c21c
hanhan <wuhanhan999@gmail.com>
2016-08-12 22:47:05 -0700

    update MLR_basics.R

--- MLR_basics.R
+++ MLR_basics.R
@@ -226,3 +226,121 @@ dt_model <- train(t.tree, trainTask)
 getLearnerModel(dt_model)
 dtpredict <- predict(dt_model, testTask)
 dtpredict
+
+# Random Forest
+getParamSet("classif.randomForest")
+rf_learner <- makeLearner("classif.randomForest", predict.type = "response", par.vals = list(ntree = 200, mtry = 3))
+rf_learner$par.vals <- list(
+  importance = TRUE
+)
+rf_param <- makeParamSet(
+  makeIntegerParam("ntree",lower = 50, upper = 500),
+  makeIntegerParam("mtry", lower = 3, upper = 10),
+  makeIntegerParam("nodesize", lower = 10, upper = 50)
+)
+## random search is faster than grid search, but may miss the real optimal
+rancontrol <- makeTuneControlRandom(maxit = 50L)
+cv_rf <- makeResampleDesc("CV",iters = 3L)
+rf_tune <- tuneParams(learner = rf_learner, resampling = cv_rf, task = trainTask, par.set = rf_param, control = rancontrol, measures = acc)
+rf_tune$x
+rf_tune$y
+rf.tree <- setHyperPars(rf_learner, par.vals = rf_tune$x)
+rf_model <- train(rf.tree, trainTask)
+getLearnerModel(t.rpart)
+rfpredict <- predict(rf_model, testTask)
+rfpredict
+
+
+# SVM
+getParamSet("classif.ksvm")
+svm_learner <- makeLearner("classif.ksvm", predict.type = "response")
+svm_param <- makeParamSet(
+  makeDiscreteParam("C", values = 2^c(-8,-4,-2,0)), #cost parameters
+  makeDiscreteParam("sigma", values = 2^c(-8,-4,0,4)) #RBF Kernel Parameter
+)
+## search function
+ctrl <- makeTuneControlGrid()
+cv_svm <- makeResampleDesc("CV",iters = 3L)
+svm_tune <- tuneParams(svm_learner, task = trainTask, resampling = cv_svm, par.set = svm_param, control = ctrl,measures = acc)
+svm_tune$x
+svm_tune$y
+t.svm <- setHyperPars(svm_learner, par.vals = svm_tune$x)
+svm_model <- train(svm_learner, trainTask)
+svmpredict <- predict(svm_model, testTask)
+svmpredict
+
+
+# GBM - after 1 round prediction, ti checks for incorrect predictions and give them weights,
+## predicting them again till they are predicted correctly
+getParamSet("classif.gbm")
+gbm_learned <- makeLearner("classif.gbm", predict.type = "response")
+rancontrol <- makeTuneControlRandom(maxit = 50L)
+cv_gbm <- makeResampleDesc("CV",iters = 3L)
+gbm_param<- makeParamSet(
+  makeDiscreteParam("distribution", values = "bernoulli"),
+  makeIntegerParam("n.trees", lower = 100, upper = 1000), #number of trees
+  makeIntegerParam("interaction.depth", lower = 2, upper = 10), #depth of tree
+  makeIntegerParam("n.minobsinnode", lower = 10, upper = 80),
+  makeNumericParam("shrinkage",lower = 0.01, upper = 1)
+)
+gbm_tune <- tuneParams(learner = gbm_learned, task = trainTask,resampling = cv_gbm,measures = acc,par.set = gbm_param,control = rancontrol)
+gbm_tune$x
+gbm_tune$y
+final_gbm <- setHyperPars(learner = gbm_learned, par.vals = gbm_tune$x)
+gbm_model <- train(final_gbm, trainTask)
+gbmpredict <- predict(gbm_model, testTask)
+
+
+## XGboost - cannot predict well on unseen data, so if the test data has no truth, XGboost may not be a good choice
+set.seed(410)
+getParamSet("classif.xgboost")
+xg_learner <- makeLearner("classif.xgboost", predict.type = "response")
+xg_learner$par.vals <- list(
+  objective = "binary:logistic",
+  eval_metric = "error",
+  nrounds = 250
+)
+
+xg_param <- makeParamSet(
+  makeIntegerParam("nrounds",lower=200,upper=600),
+  makeIntegerParam("max_depth",lower=3,upper=20),
+  makeNumericParam("lambda",lower=0.55,upper=0.60),
+  makeNumericParam("eta", lower = 0.001, upper = 0.5),
+  makeNumericParam("subsample", lower = 0.10, upper = 0.80),
+  makeNumericParam("min_child_weight",lower=1,upper=5),
+  makeNumericParam("colsample_bytree",lower = 0.2,upper = 0.8)
+)
+rancontrol <- makeTuneControlRandom(maxit = 100L)
+cv_xg <- makeResampleDesc("CV",iters = 3L)
+xg_tune <- tuneParams(learner = xg_learner, task = trainTask, resampling = cv_xg,measures = acc,par.set = xg_param, control = rancontrol)
+xg_final <- setHyperPars(learner = xg_learner, par.vals = xg_tune$x)
+xgmodel <- train(xg_final, trainTask)
+xgpredict <- predict(xgmodel, testTask)
+
+
+# random forest with top 6 features
+## so far random forest is the best model here, train the data with top 6 features with random forest
+getParamSet("classif.randomForest")
+top_task <- filterFeatures(trainTask, method = "rf.importance", abs = 6)
+
+rf_learner <- makeLearner("classif.randomForest", predict.type = "response", par.vals = list(ntree = 200, mtry = 3))
+rf_learner$par.vals <- list(
+  importance = TRUE
+)
+rf_param <- makeParamSet(
+  makeIntegerParam("ntree",lower = 50, upper = 500),
+  makeIntegerParam("mtry", lower = 3, upper = 10),
+  makeIntegerParam("nodesize", lower = 10, upper = 50)
+)
+## random search is faster than grid search, but may miss the real optimal
+rancontrol <- makeTuneControlRandom(maxit = 50L)
+cv_rf <- makeResampleDesc("CV",iters = 3L)
+rf_tune <- tuneParams(learner = rf_learner, resampling = cv_rf, task = top_task, par.set = rf_param, control = rancontrol, measures = acc)
+rf_tune$x
+rf_tune$y
+rf.tree <- setHyperPars(rf_learner, par.vals = rf_tune$x)
+rf_model <- train(rf.tree, top_task)
+getLearnerModel(t.rpart)
+rfpredict <- predict(rf_model, testTask)
+rfpredict
+result <- data.frame(test$Loan_ID, rfpredict$data$response)
