0ec77c1f53b125de619a9b39d64cea41c4b6ecad
hanhan <wuhanhan999@gmail.com>
2016-09-25 23:16:55 -0700

    update deal_with_imbalanced_data_2.R

--- deal_with_imbalanced_data_2.R
+++ deal_with_imbalanced_data_2.R
@@ -67,4 +67,47 @@ tr(num_train$num_person_Worked_employer)
 tr(num_train$weeks_worked_in_year)
 
 
+# In classification proble, it may help determine clusters when plotting the target
+## with numeric variables
+ggplot(data=num_train,aes(x = age, y=wage_per_hour))+geom_point(aes(colour=cat_train$income_level))+scale_y_continuous("wage per hour", breaks = seq(0,10000,1000))
+
+## for the target with categorical variable, we could use bar chart
+all_bar <- function(i){
+  ggplot(cat_train,aes(x=i,fill=income_level))+geom_bar(position = "dodge",  color="black")+scale_fill_brewer(palette = "Pastel1")+theme(axis.text.x =element_text(angle  = 60,hjust = 1,size=10))
+}
+
+all_bar(cat_train$class_of_worker)
+all_bar(cat_train$education)
+# or use proportional table to check the target and the categorical variable
+prop.table(table(cat_train$marital_status,cat_train$income_level),1)
+prop.table(table(cat_train$class_of_worker,cat_train$income_level),1)
+
+
+# data cleaning
+
+## find missing data in numerical variables, no missing data in this case
+table(is.na(num_train))
+table(is.na(num_test))
+
+## check correlation between numeric data
+library(caret)
+ax <-findCorrelation(x = cor(num_train), cutoff = 0.7)   # 0.7 is the threshold here
+str(num_train)
+num_train <- num_train[,-ax,with=FALSE] 
+str(num_train)   # removed weeks_worked_in_year
+num_test[,weeks_worked_in_year := NULL]
+str(num_test)
+
+## find missing data in categorical variables
+mvtr <- sapply(cat_train, function(x){sum(is.na(x))/length(x)})*100
+mvtr
+mvte <- sapply(cat_test, function(x){sum(is.na(x))/length(x)})*100
+mvte
+#select columns with missing value less than 5%, 
+## in this case, both training and testing data will remove the same columns
+cat_train <- subset(cat_train, select = mvtr < 5 )
+head(cat_train)
+cat_test <- subset(cat_test, select = mvtr < 5 )
+
+
 # TO BE CONTINUED....
