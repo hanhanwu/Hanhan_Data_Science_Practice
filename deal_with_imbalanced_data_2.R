70c0bf22a5ef8e7bd26380f987a5e5da59a7bd76
hanhan <wuhanhan999@gmail.com>
2016-09-26 23:26:03 -0700

    update deal_with_imbalanced_data_2.R

--- deal_with_imbalanced_data_2.R
+++ deal_with_imbalanced_data_2.R
@@ -60,6 +60,7 @@ tr <- function(a){
 
 str(num_train)
 tr(num_train$age)
+tr(num_train$wage_per_hour)
 tr(num_train$capital_gains)
 tr(num_train$capital_losses)
 tr(num_train$dividend_from_Stocks)
@@ -67,7 +68,7 @@ tr(num_train$num_person_Worked_employer)
 tr(num_train$weeks_worked_in_year)
 
 
-# In classification proble, it may help determine clusters when plotting the target
+# In classification problem, it may help determine clusters when plotting the target
 ## with numeric variables
 ggplot(data=num_train,aes(x = age, y=wage_per_hour))+geom_point(aes(colour=cat_train$income_level))+scale_y_continuous("wage per hour", breaks = seq(0,10000,1000))
 
@@ -103,11 +104,70 @@ mvtr <- sapply(cat_train, function(x){sum(is.na(x))/length(x)})*100
 mvtr
 mvte <- sapply(cat_test, function(x){sum(is.na(x))/length(x)})*100
 mvte
-#select columns with missing value less than 5%, 
+#select columns with missing value than 5%, 
 ## in this case, both training and testing data will remove the same columns
 cat_train <- subset(cat_train, select = mvtr < 5 )
 head(cat_train)
 cat_test <- subset(cat_test, select = mvtr < 5 )
+## to deal with the rest of missing data columns, convter NA to "Unavailable" (this solution, looks silly)
+# convert to character
+cat_train <- cat_train[,names(cat_train) := lapply(.SD, as.character),.SDcols = names(cat_train)]
+for (i in seq_along(cat_train)) set(cat_train, i=which(is.na(cat_train[[i]])), j=i, value="Unavailable")
+#convert back to factors
+cat_train <- cat_train[, names(cat_train) := lapply(.SD,factor), .SDcols = names(cat_train)]
+unique(cat_train$country_father)
+
+# convert to character
+cat_test <- cat_test[,names(cat_test) := lapply(.SD, as.character),.SDcols = names(cat_test)]
+for (i in seq_along(cat_test)) set(cat_test, i=which(is.na(cat_test[[i]])), j=i, value="Unavailable")
+#convert back to factors
+cat_test <- cat_test[, names(cat_test) := lapply(.SD,factor), .SDcols = names(cat_test)]
+unique(cat_test$country_father)
 
 
-# TO BE CONTINUED....
+# deal with imbalanced data - combine levels with < 5% values in an imbalanced variable as "Other"
+for(i in names(cat_train)){
+  p <- 5/100
+  ld <- names(which(prop.table(table(cat_train[[i]])) < p))
+  levels(cat_train[[i]])[levels(cat_train[[i]]) %in% ld] <- "Other"
+}
+
+for(i in names(cat_test)){
+  p <- 5/100
+  ld <- names(which(prop.table(table(cat_test[[i]])) < p))
+  levels(cat_test[[i]])[levels(cat_test[[i]]) %in% ld] <- "Other"
+}
+
+
+# check if categorical variables in training and testing data have mimatched levels
+library(mlr)
+summarizeColumns(cat_train)[,"nlevs"]
+summarizeColumns(cat_test)[,"nlevs"]
+
+
+
+
+# binning numerical variables to deal with data imbalance
+
+# library(rpart)
+# str(num_train)
+# fit <- rpart(cat_train$income_level~ age+wage_per_hour+capital_gains+capital_losses+dividend_from_Stocks+num_person_Worked_employer, method="class", data=num_train)
+# printcp(fit) # display the results 
+# plotcp(fit) # visualize cross-validation results 
+# summary(fit) # detailed summary of splits
+
+num_train[,.N,age][order(age)]
+tr(num_train$age)  # based on the plot, create 3 bins, 0-25, 26-50, 51-90
+num_train[,age:= cut(x = age,breaks = c(0,25,50,90),include.lowest = TRUE,labels = c("young","adult","old"))]
+num_train[,age := factor(age)]
+unique(num_train$age)
+
+num_test[,age:= cut(x = age,breaks = c(0,25,50,90),include.lowest = TRUE,labels = c("young","adult","old"))]
+num_test[,age := factor(age)]
+unique(num_test$age)
+
+
+num_train[,.N,wage_per_hour][order(-N)]
+  
+  
+# TO BE CONTINUED...
