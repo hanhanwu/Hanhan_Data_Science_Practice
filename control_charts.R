524f865e82218931281d10273e9fe030f9bf8d4e
hanhan <hanhanwu@users.noreply.github.com>
2017-05-16 22:34:08 -0700

    update control_charts.R

--- control_charts.R
+++ control_charts.R
@@ -68,10 +68,21 @@ qic(n.pat.pu,
     xlab     = 'Week')
 
 
-## G Chart
-# Create vector of random values from a geometric distribution
-d <- c(NA, rgeom(23, 0.08))
+## prime control chart - use when control limits for U, P charts are too narrow
+qic(n.pat.pu, discharges, week, d,
+    chart    = 'p',
+    multiply = 100,
+    main     = 'Prime P chart of patients with pressure ulcer',
+    ylab     = 'Percent',
+    xlab     = 'Week',
+    prime    = TRUE)
+
+
+## G Chart - geometric distribution, displays the number of cases between events
+# Create vector of random values that have a geometric distribution
+d <- c(NA, rgeom(100, 0.08))
 d
+
 qic(d,
     chart = 'g',
     main  = 'Patients between pressure ulcers (G chart)',
@@ -79,4 +90,80 @@ qic(d,
     xlab  = 'Discharge no.')
 
 
-# TO BE CONTINUED...
+## T Chart - displays the time between events
+dates  <- seq(as.Date('2017-1-1'), as.Date('2017-12-31'), by = 'day')
+events <- sort(sample(dates, 100))
+events
+d <- c(NA, diff(events))
+d
+
+qic(d,
+    chart = 't',
+    main  = 'Days between pressure ulcers (T chart)',
+    ylab  = 'Days',
+    xlab  = 'Pressure ulcer no.')
+
+
+## I and MR Charts - individual measures (I think it means individual feature)
+# I chart is often accompained with MR chart, which measures the moving range (absolute difference between neughboring data)
+# Vector of birth weights from 100 babies
+y <- round(rnorm(100, mean = 3400, sd = 400))
+y
+
+qic(y,
+    chart = 'i',
+    main  = 'Birth weight (I chart)',
+    ylab  = 'Grams',
+    xlab  = 'Baby no.')
+qic(y,
+    chart = 'mr',
+    main  = 'Pairwise differences in birth weights (MR chart)',
+    ylab  = 'Grams',
+    xlab  = 'Baby no.')
+
+
+## Xbar and S chart
+# Xbar - shows the average of a column
+# S chart - shows the standard deviation of a column
+# Vector of 100 subgroup sizes (average = 12)
+sizes <- rpois(100, 12)
+sizes
+
+# Vector of dates identifying subgroups
+date <- seq(as.Date('2017-1-1'), length.out = 100, by = 'day')
+date <- rep(date, sizes)
+date
+
+# Vector of birth weights
+y <- round(rnorm(sum(sizes), 3400, 400))
+y
+
+# Data frame of birth weights and dates
+d <- data.frame(y, date)
+d
+
+qic(y, 
+    x     = date, 
+    data  = d,
+    chart = 'xbar',
+    main  = 'Average birth weight (Xbar chart)',
+    ylab  = 'Grams',
+    xlab  = 'Date')
+qic(y, 
+    x = date, 
+    data = d,
+    chart = 's',
+    main = 'Standard deviation of birth weight (S chart)',
+    ylab = 'Grams',
+    xlab = 'Date')
+
+
+## standardized a control chart - creates a standardised control chart, where points are plotted in standard deviation units along with a center line at zero and control limits at 3 and -3. Only relevant for P, U and Xbar charts.
+qic(y, 
+    x     = date, 
+    data  = d,
+    chart = 'xbar',
+    standardised = T,
+    main  = 'Average birth weight (Xbar chart)',
+    ylab  = 'Grams',
+    xlab  = 'Date')
