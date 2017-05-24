3d8f960cd900930fdd2446b3067b23c656e0fb7b
hanhan <hanhanwu@users.noreply.github.com>
2017-05-23 23:46:00 -0700

    update time_series_beginner.R

--- sequencial_analysis/time_series_beginner.R
+++ sequencial_analysis/time_series_beginner.R
@@ -9,8 +9,17 @@ ncol(jj)
 jjm <- as.matrix(jj)
 dim(jjm)
 head(jj)
+time(jj)
+cycle(jj)
+plot(jj, ylab="Earnings per Share", main="J & J")
 
 # normal distribution for 99 random numbers
 # start from 77th row, April
-# display 12 months
+# display 12 months (if you set frequency as 4, it will play quarterly data)
 (zardoz = ts(rnorm(99), start=c(77,4), frequency=12))   
+(oz = window(zardoz, start=77, end=c(99,12)))   # use window to get part of the time series data
+time(zardoz)
+cycle(zardoz)
+plot(zardoz, ylab="Random Value", main="Zardoz")
+
+# too busy recently, TO BE CONTINUED...
