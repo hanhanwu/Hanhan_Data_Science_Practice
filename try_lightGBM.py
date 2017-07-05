4dc87a451a28aca0cd5dd29f5d880b457d0fe500
hanhan <hanhanwu@users.noreply.github.com>
2017-07-05 00:13:34 -0700

    update try_lightGBM.py

--- try_lightGBM.py
+++ try_lightGBM.py
@@ -4,8 +4,10 @@ from pandas import Series, DataFrame
 import lightgbm as lgb 
 import xgboost as xgb 
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+from sklearn.model_selection import train_test_split 
 
-data=pd.read_csv('adult_train.csv',header=None) 
+
+data=pd.read_csv('adult.csv',header=None) 
 # assign column names to the data
 data.columns=['age','workclass','fnlwgt','education','education-num','marital_Status','occupation','relationship','race','sex','capital_gain','capital_loss','hours_per_week','native_country','Income'] 
 data.head()
@@ -29,3 +31,19 @@ one_hot_native_country=pd.get_dummies(data.native_country)
 
 data.drop(['workclass','education','marital_Status','occupation','relationship','race','sex','native_country'],axis=1,inplace=True) 
 data=pd.concat([data,one_hot_workclass,one_hot_education,one_hot_marital_Status,one_hot_occupation,one_hot_relationship,one_hot_race,one_hot_sex,one_hot_native_country],axis=1) 
+
+#removing dulpicate columns 
+i = np.unique(data.columns, return_index=True) 
+i[1]  # index of unique columns
+data=data.iloc[:, i[1]]  # use the index of unique columns
+data.head()
+
+# seperate features and the label
+features = data.drop('Income',axis=1) 
+label = data.Income 
+label.mode()[0]
+label.fillna(label.mode()[0],inplace=True)  # impute missing data with mode
+label.value_counts()
+
+# split into training and testing data
+features_train,features_test,label_train,label_test=train_test_split(features,label,test_size=.3)
