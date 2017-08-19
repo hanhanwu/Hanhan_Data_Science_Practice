5a4537068ae230457b4c53effa37496c560bccb7
hanhan <hanhanwu@users.noreply.github.com>
2017-08-19 00:04:19 -0700

    create basic_alg_apriori.R

---
+++ Frequent_Pattern_Mining/basic_alg_apriori.R
@@ -0,0 +1,32 @@
+# review Apriori here
+
+library(arules)
+library(arulesViz)
+library(mlr)
+library(RColorBrewer)
+
+data("Groceries")
+str(Groceries)
+summarizeColumns(Groceries@itemInfo)
+head(Groceries@itemInfo)    # from level 1 to level 2 to labels, the categories become more detailed
+summary(Groceries)
+
+rules <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.79))
+inspect(rules[1:10])   # top 10 association rules
+
+
+# plot relative item frequency
+## you can place items around most frequently purchased products to increase their sales
+arules::itemFrequencyPlot(Groceries,topN=20,
+                          col=brewer.pal(8,'Set3'),   # create some nice color
+                          main='Relative Item Frequency Plot',
+                          type="relative",ylab="Item Frequency (Relative)")
+
+
+# plot associate between products
+## node size represent Support level
+## color of nodes represent Life ratios
+## you can put those strongly associated products together
+plot(rules[1:10],
+     method = "graph",
+     control = list(type = "items"))
